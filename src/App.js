49c07634f6997da54d3a9af9122944d88c1c853d
TedisAgolli <tedisagolli@gmail.com>
2021-03-20 15:11:39 -0400

    Insert token and get first highlight

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+const HIGHLIGHTS_ROUTE = "https://readwise.io/api/v2/highlights";
 function App() {
+  const [quote, setQuote] = useState("");
+  const [token, setToken] = useState("");
+  async function getHighlights(e) {
+    e.preventDefault();
+    const data = await fetch(HIGHLIGHTS_ROUTE, {
+      method: "GET",
+      mode: "cors",
+      headers: { Authorization: `TOKEN ${token}` },
+    })
+      .then((res) => res.json())
+      .then((res) => res.results[0].text)
+      .catch((e) => console.log(e));
+    setQuote(data);
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App m-2">
+      <form className="center max-w-2xl" onSubmit={getHighlights}>
+        <div>
+          <label
+            htmlFor="token"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Token
+          </label>
+          <div className="mt-1">
+            <input
+              type="text"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              id="token"
+              name="token"
+              onChange={(e) => setToken(e.target.value)}
+              value={token}
+            />
+          </div>
+        </div>
+        <button className="bg-blue-100 rounded p-2 m-1">Get highlights</button>
+      </form>
+      <p>{quote}</p>
     </div>
   );
 }
